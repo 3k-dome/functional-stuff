5cd13d5d79012858704fb7618a602bda57fab898
3k-dome <doenerfresser3k@gmail.com>
2025-02-24 20:37:45 +0100

    reworks the result monad
    error typing is gone temporarily

--- src/functional_stuff/monads/__init__.py
+++ src/functional_stuff/monads/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ("Error", "Maybe", "Nothing", "Ok", "Result", "Some", "maybe", "to_result")
+__all__ = ("Error", "Maybe", "Nothing", "Ok", "Result", "Some", "maybe", "result")
 
 from functional_stuff.monads.maybe import Maybe, Nothing, Some, maybe
-from functional_stuff.monads.result import Error, Ok, Result, to_result
+from functional_stuff.monads.result import Error, Ok, Result, result
