1135a6a461b7fad17c5be53a7c57edac5208026d
3k-dome <doenerfresser3k@gmail.com>
2025-02-24 20:10:33 +0100

    reduces and simplifies base types
    deferred api will be reworked, focus on a solid base for now

--- src/functional_stuff/monads/base.py
+++ src/functional_stuff/monads/base.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from collections.abc import Callable, Coroutine
+from collections.abc import Callable
 from typing import Any, Generic, TypeVar
 
 T = TypeVar("T", bound=object)
@@ -7,45 +7,22 @@ U = TypeVar("U", bound=object)
 
 
 class AbstractMonad(ABC, Generic[T]):
-    @abstractmethod
-    def bind(self, func: Callable[[T], U]) -> "AbstractMonad[U]": ...
+    """Abstract base for monadic types."""
 
     @abstractmethod
-    def join(self: "AbstractMonad[AbstractMonad[U]]") -> "AbstractMonad[U]": ...
-
-
-M = TypeVar("M", bound=AbstractMonad[Any])
-
-
-class AbstractAsyncMonad(ABC, Generic[T]):
-    """A monadic type that allows async bindings."""
+    def map(self, func: Callable[[T], U]) -> "AbstractMonad[U]":
+        """Apply a function to the value inside the monad and return a new monad of same context."""
+        ...
 
     @abstractmethod
-    async def bind_async(self, func: Callable[[T], Coroutine[Any, Any, U]]) -> "AbstractMonad[U]": ...
-
-
-class AbstractDeferrableMonad(ABC, Generic[T]):
-    """A monadic type that allows deferred bindings."""
+    def bind(self, func: Callable[[T], "AbstractMonad[U]"]) -> "AbstractMonad[U]":
+        """Apply a monadic function of same context to the value inside the monad and return is result."""
+        ...
 
     @abstractmethod
-    def bind_deferred(self, func: Callable[[T], Coroutine[Any, Any, U]]) -> "AbstractDeferredMonad[T, T, U]": ...
+    def join(self: "AbstractMonad[MonadT]") -> "MonadT":
+        """Flattens a monad of monads into a single monad."""
+        ...
 
 
-V = TypeVar("V", bound=object)
-W = TypeVar("W", bound=object)
-
-
-class AbstractDeferredMonad(ABC, Generic[T, U, V]):
-    """A deferred monadic type.
-
-    A deferred monad tracks its bindings and executes them in serial using a single `wait` call.
-
-    The generics `T`, `U` and `V` refer to the held monads generic type, the result type of the *previous
-    deferred* binding and the result type of the *latest deferred* binding respectively.
-    """
-
-    @abstractmethod
-    def bind(self, awaitable: Callable[[V], Coroutine[None, None, W]]) -> "AbstractDeferredMonad[T, V, W]": ...
-
-    @abstractmethod
-    async def wait(self) -> AbstractMonad[Any]: ...
+MonadT = TypeVar("MonadT", bound="AbstractMonad[Any]")
