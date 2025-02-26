552711cb934c8af35d4ee60dce2393c70377141f
3k-dome <doenerfresser3k@gmail.com>
2025-02-26 22:01:08 +0100

    adds conversion methods

--- src/functional_stuff/monads/enumerable.py
+++ src/functional_stuff/monads/enumerable.py
@@ -1,9 +1,12 @@
+from collections import deque
 from collections.abc import Callable, Iterable, Iterator
 from dataclasses import dataclass
 from itertools import chain
-from typing import Any, TypeVar, cast, final
+from typing import Any, TypeVar, cast, final, overload
 
 from functional_stuff.monads.base import AbstractMonad, T, U
+
+K = TypeVar("K", bound=object)
 
 
 @final
@@ -35,6 +38,35 @@ class Enumerable(Iterable[T], AbstractMonad[T]):
     def select_many(self, selector: Callable[[T], Iterable[U]]) -> "Enumerable[U]":
         """Projects each value of the underlying iterable into a new form of `U` and flattens the result."""
         return Enumerable(chain.from_iterable(selector(x) for x in self))
+
+    def to_deque(self) -> deque[T]:
+        """Creates a new `deque[T]` by consuming the underlying iterable."""
+        return deque(self)
+
+    def to_list(self) -> list[T]:
+        """Creates a new `list[T]` by consuming the underlying iterable."""
+        return list(self)
+
+    def to_set(self) -> set[T]:
+        """Creates a new `set[T]` by consuming the underlying iterable."""
+        return set(self)
+
+    @overload
+    def to_dict(self, key: Callable[[T], K]) -> dict[K, T]: ...
+
+    @overload
+    def to_dict(self, key: Callable[[T], K], func: None) -> dict[K, T]: ...
+
+    @overload
+    def to_dict(self, key: Callable[[T], K], func: Callable[[T], U]) -> dict[K, U]: ...
+
+    def to_dict(self, key: Callable[[T], K], func: Callable[[T], U] | None = None) -> dict[K, U] | dict[K, T]:
+        """Creates a new `dict[K, U]` or `dict[K, T]` by consuming the underlying iterable."""
+        match func:
+            case Callable():
+                return {key(x): func(x) for x in self}
+            case _:
+                return {key(x): x for x in self}
 
 
 EnumerableT = TypeVar("EnumerableT", bound=Enumerable[Any])
