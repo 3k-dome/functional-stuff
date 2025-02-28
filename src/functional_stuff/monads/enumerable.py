9c6708f7d6497a73897964f414a2efcc27252aeb
3k-dome <doenerfresser3k@gmail.com>
2025-02-28 23:23:22 +0100

    adds an enumerable decorator

--- src/functional_stuff/monads/enumerable.py
+++ src/functional_stuff/monads/enumerable.py
@@ -1,12 +1,17 @@
+__all__ = ("Enumerable", "enumerable")
+
+
 from collections import deque
 from collections.abc import Callable, Iterable, Iterator
 from dataclasses import dataclass
+from functools import wraps
 from itertools import chain
-from typing import Any, TypeVar, cast, final, overload
+from typing import Any, ParamSpec, TypeVar, cast, final, overload
 
 from functional_stuff.monads.base import AbstractMonad, T, U
 
 K = TypeVar("K", bound=object)
+P = ParamSpec("P")
 Predicate = Callable[[T], bool]
 
 
@@ -75,3 +80,25 @@ class Enumerable(Iterable[T], AbstractMonad[T]):
 
 
 EnumerableT = TypeVar("EnumerableT", bound=Enumerable[Any])
+
+
+@overload
+def enumerable(func: Callable[P, Iterable[T]]) -> Callable[P, Enumerable[T]]: ...
+
+
+@overload
+def enumerable(func: Callable[P, Iterable[T] | None]) -> Callable[P, Enumerable[T] | None]: ...
+
+
+def enumerable(func: Callable[P, Iterable[T] | None]) -> Callable[P, Enumerable[T] | None]:
+    """Wraps the result of a function in an `Enumerable` monad."""
+
+    @wraps(func)
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> Enumerable[T] | None:
+        match result := func(*args, **kwargs):
+            case Iterable():
+                return Enumerable(result)
+            case _:
+                return result
+
+    return wrapper
