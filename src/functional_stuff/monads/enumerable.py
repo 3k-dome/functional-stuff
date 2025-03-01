354661db7033c152e459386ccd96714a64b8a65e
3k-dome <doenerfresser3k@gmail.com>
2025-03-01 11:34:54 +0100

    adds any and all methods
    unfreezes the instance to allow changes to the underling iterable without workarounds, marks it as private

--- src/functional_stuff/monads/enumerable.py
+++ src/functional_stuff/monads/enumerable.py
@@ -3,10 +3,10 @@ __all__ = ("Enumerable", "enumerable")
 
 from collections import deque
 from collections.abc import Callable, Iterable, Iterator
-from dataclasses import dataclass
+from dataclasses import InitVar, dataclass, field
 from functools import wraps
-from itertools import chain
-from typing import Any, ParamSpec, TypeVar, cast, final, overload
+from itertools import chain, tee
+from typing import Any, Concatenate, ParamSpec, TypeVar, cast, final, overload
 
 from functional_stuff.monads.base import AbstractMonad, T, U
 
@@ -15,12 +15,43 @@ P = ParamSpec("P")
 Predicate = Callable[[T], bool]
 
 
+def preserve(func: Callable[Concatenate["Enumerable[T]", P], U]) -> Callable[Concatenate["Enumerable[T]", P], U]:
+    """Enables a consuming method to preserve its elements if `preserve=True` is passed.
+
+    Replaces the underlying iterable with `functools.tee` if it is an `Iterator` (i.e. items
+    will probably get consumed) and `preserve=True`, otherwise assumes that the inner iterable
+    is some for of non consuming collection type and does nothing.
+    """
+
+    @wraps(func)
+    def wrapper(instance: "Enumerable[T]", *args: P.args, **kwargs: P.kwargs) -> U:
+        preserve = kwargs.get("preserve")
+        match (instance._iterable, preserve):  # pyright: ignore[reportPrivateUsage] # noqa: SLF001
+            case (Iterator() as iterable, True):
+                consumed, preserved = tee(iterable)
+                instance._iterable = consumed  # pyright: ignore[reportPrivateUsage] # noqa: SLF001
+                result = func(instance, *args, **kwargs)
+                instance._iterable = preserved  # pyright: ignore[reportPrivateUsage] # noqa: SLF001
+                return result
+            case _:
+                return func(instance, *args, **kwargs)
+
+    return wrapper
+
+
 @final
-@dataclass(slots=True, frozen=True)
+@dataclass(slots=True)
 class Enumerable(Iterable[T], AbstractMonad[T]):
     """A LINQ inspired `Iterable` over type `T`."""
 
-    iterable: Iterable[T] = tuple[T]()
+    iterable: InitVar[Iterable[T]] = tuple[T]()
+    _iterable: Iterable[T] = field(init=False)
+
+    def __post_init__(self, iterable: Iterable[T]) -> None:
+        self._iterable = iterable
+
+    def __iter__(self) -> Iterator[T]:
+        return iter(self._iterable)
 
     def map(self, func: Callable[[T], U]) -> "Enumerable[U]":
         """Proxies `select`."""
@@ -34,9 +65,6 @@ class Enumerable(Iterable[T], AbstractMonad[T]):
         """Proxies `select_many` for inner `Enumerable`s using an identity function."""
         return cast("EnumerableT", self.select_many(lambda x: x))
 
-    def __iter__(self) -> Iterator[T]:
-        return iter(self.iterable)
-
     def select(self, selector: Callable[[T], U]) -> "Enumerable[U]":
         """Projects each value of the underlying iterable into a new form of `U`."""
         return Enumerable(selector(x) for x in self)
@@ -48,6 +76,31 @@ class Enumerable(Iterable[T], AbstractMonad[T]):
     def where(self, predicate: Predicate[T]) -> "Enumerable[T]":
         """Filters the underlying iterable base on `predicate`."""
         return Enumerable(x for x in self if predicate(x))
+
+    @overload
+    def any(self, predicate: None, *, preserve: bool = False) -> bool: ...
+
+    @overload
+    def any(self, predicate: None = None, *, preserve: bool = False) -> bool: ...
+
+    @overload
+    def any(self, predicate: Predicate[T], *, preserve: bool = False) -> bool: ...
+
+    @preserve
+    def any(self, predicate: Predicate[T] | None = None, *, preserve: bool = False) -> bool:  # noqa: ARG002
+        """Return True if any element satisfies the predicate or if any element exists."""
+        match predicate:
+            case Callable():
+                return any(predicate(x) for x in self)
+            case _:
+                for _ in self:
+                    return True
+                return False
+
+    @preserve
+    def all(self, predicate: Predicate[T], *, preserve: bool = False) -> bool:  # noqa: ARG002
+        """Return True if all elements satisfy the predicate."""
+        return all(predicate(x) for x in self)
 
     def to_deque(self) -> deque[T]:
         """Creates a new `deque[T]` by consuming the underlying iterable."""
